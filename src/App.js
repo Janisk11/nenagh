150ed69ea306c690f2cbef723d7e24cdd6778ce4
Janisk11 <janiskjoseph@gmail.com>
2024-09-18 17:02:15 +0100

    scroll to top on each page load

--- src/App.js
+++ src/App.js
@@ -16,43 +16,47 @@ import Backdrop from './components/navbar/backdrop';
 import Footer from './components/footer/footer';
 
 const App = () => {
-  const [userIsScrolled, setUserIsScrolled] = useState(false);
-  const [mobileNavbarOpen, setMobileNavbarOpen] = useState(false);
+  const [userIsScrolled, setUserIsScrolled] = useState(false)
+  const [mobileNavbarOpen, setMobileNavbarOpen] = useState(false)
+  const location = useLocation()
 
   const userIsScrolledHandler = useCallback(() => {
-    setUserIsScrolled(window.pageYOffset > 80);
-  }, []);
+    setUserIsScrolled(window.pageYOffset > 80)
+  }, [])
 
   useEffect(() => {
-    window.addEventListener('scroll', userIsScrolledHandler);
+    window.addEventListener('scroll', userIsScrolledHandler)
     return () => {
-      window.removeEventListener('scroll', userIsScrolledHandler);
-    };
-  }, [userIsScrolledHandler]);
+      window.removeEventListener('scroll', userIsScrolledHandler)
+    }
+  }, [userIsScrolledHandler])
 
-  const closeMobileMenu = () => setMobileNavbarOpen(false);
-  const openMobileMenu = () => setMobileNavbarOpen(true);
-  const location = useLocation()
-  const currentPath = location.pathname
+  // Scroll to top on route change
+  useEffect(() => {
+    window.scrollTo(0, 0) // Scrolls to top of the page
+  }, [location.pathname]) // Triggered whenever the route (path) changes
+
+  const closeMobileMenu = () => setMobileNavbarOpen(false)
+  const openMobileMenu = () => setMobileNavbarOpen(true)
 
   return (
     <div className="App">
       <MobileNav
         isOpen={mobileNavbarOpen}
         closeMobileMenu={closeMobileMenu}
-        currentPath={currentPath}
+        currentPath={location.pathname}
       />
       <Backdrop closeMobileMenu={closeMobileMenu} isOpen={mobileNavbarOpen} />
       <DesktopNav
         userIsScrolled={userIsScrolled}
         mobileMenuOpen={openMobileMenu}
-        currentPath={currentPath}
+        currentPath={location.pathname}
       />
       <Element name="top" className="top-element"></Element>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/events" element={<Events />}></Route>
-        <Route path="/contactus" element={<ContactUs/>}></Route>
+        <Route path="/contactus" element={<ContactUs />}></Route>
       </Routes>
       <Footer />
     </div>
